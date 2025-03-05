03b118fba15e965853820cfaebc3b50fa59b7d71
Afiqgsr <afiqgaluhsetyar@gmail.com>
2025-03-05 23:46:31 +0700

    Membuat fungsi di ScoreViewModel

--- app/src/main/java/com/example/minggu_3/ScoreViewModel.kt
+++ app/src/main/java/com/example/minggu_3/ScoreViewModel.kt
@@ -6,10 +6,11 @@ class ScoreViewModel:ViewModel() {
     var scoreA: Int = 0
     var scoreB: Int = 0
 
+    // Fungsi untuk menambah skor Tim A sebanyak 1
     fun incrementScoreA(){
         scoreA++
     }
-
+    // Fungsi untuk menambah skorr tim B sebanyak 1
     fun incrementScoreB(){
         scoreB++
     }
